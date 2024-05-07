eb9e897756272159c2f7cb25952ea23d3d86f453
yashmaggu <yashmaggu299@gmail.com>
2024-05-07 14:09:57 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import cors from 'cors'
 import  express  from 'express';
 
@@ -13,8 +14,8 @@ import connecttomongo from './db/connecttomongo.js';
 import cookieParser from 'cookie-parser';
 
 import { app ,server}  from './socket/socket.js'
-const PORT=8000;
-
+const PORT=8000 || process.env.PORT;
+const __dirname=path.resolve();
 
 app.use(cors({
          origin:process.env.CORS_ORIGIN,
@@ -24,14 +25,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,resp)=>{
-resp.send("Server is ready")
-})
-
-
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/user",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 
 server.listen(PORT,()=>{
